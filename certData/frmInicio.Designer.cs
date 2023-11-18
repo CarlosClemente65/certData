6500fac6af8c9f7fcc4c1d8eb0255d256f9c6500
Carlos Clemente <carlos.clemente.abc@gmail.com>
2023-11-18 01:56:42 +0100

    Primera version v1.0

---
+++ certData/frmInicio.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace certData
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicio));
+            this.ofdSeleccion = new System.Windows.Forms.OpenFileDialog();
+            this.txtSeleccion = new System.Windows.Forms.TextBox();
+            this.txtPassword1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnSeleccion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ofdSeleccion
+            // 
+            this.ofdSeleccion.Filter = "(fichero.pfx)|*.pfx|(fichero.p12)|*.p12";
+            this.ofdSeleccion.Title = "Seleccionar fichero a importar";
+            // 
+            // txtSeleccion
+            // 
+            this.txtSeleccion.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtSeleccion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSeleccion.Enabled = false;
+            this.txtSeleccion.Location = new System.Drawing.Point(13, 41);
+            this.txtSeleccion.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSeleccion.Multiline = true;
+            this.txtSeleccion.Name = "txtSeleccion";
+            this.txtSeleccion.ReadOnly = true;
+            this.txtSeleccion.Size = new System.Drawing.Size(582, 25);
+            this.txtSeleccion.TabIndex = 0;
+            // 
+            // txtPassword1
+            // 
+            this.txtPassword1.Location = new System.Drawing.Point(158, 82);
+            this.txtPassword1.Name = "txtPassword1";
+            this.txtPassword1.Size = new System.Drawing.Size(140, 23);
+            this.txtPassword1.TabIndex = 2;
+            this.txtPassword1.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Clave del certificado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(347, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Repetir clave";
+            // 
+            // txtPassword2
+            // 
+            this.txtPassword2.Location = new System.Drawing.Point(455, 83);
+            this.txtPassword2.Name = "txtPassword2";
+            this.txtPassword2.Size = new System.Drawing.Size(140, 23);
+            this.txtPassword2.TabIndex = 4;
+            this.txtPassword2.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 14);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Seleccionar certificado";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnCargar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCargar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnCargar.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnCargar.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCargar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.Image = global::certData.Properties.Resources.cargar;
+            this.btnCargar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCargar.Location = new System.Drawing.Point(468, 125);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Padding = new System.Windows.Forms.Padding(5);
+            this.btnCargar.Size = new System.Drawing.Size(127, 51);
+            this.btnCargar.TabIndex = 7;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCargar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnCargar.UseVisualStyleBackColor = false;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnSeleccion
+            // 
+            this.btnSeleccion.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSeleccion.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSeleccion.BackgroundImage = global::certData.Properties.Resources.buscar;
+            this.btnSeleccion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSeleccion.FlatAppearance.BorderSize = 0;
+            this.btnSeleccion.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSeleccion.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSeleccion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSeleccion.Location = new System.Drawing.Point(568, 43);
+            this.btnSeleccion.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSeleccion.Name = "btnSeleccion";
+            this.btnSeleccion.Size = new System.Drawing.Size(25, 20);
+            this.btnSeleccion.TabIndex = 1;
+            this.btnSeleccion.UseVisualStyleBackColor = false;
+            this.btnSeleccion.Click += new System.EventHandler(this.btnSeleccion_Click);
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 205);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword1);
+            this.Controls.Add(this.btnSeleccion);
+            this.Controls.Add(this.txtSeleccion);
+            this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "frmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Seleccion certificado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog ofdSeleccion;
+        private System.Windows.Forms.TextBox txtSeleccion;
+        private System.Windows.Forms.Button btnSeleccion;
+        private System.Windows.Forms.TextBox txtPassword1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCargar;
+    }
+}
+
---
+++ certData/frmInicio.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Security.Cryptography;
+using System.Security.Permissions;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace certData
+{
+    public partial class frmInicio : Form
+    {
+        string certificadoPath = string.Empty;
+        string salida = "certData.txt";
+        string password = string.Empty;
+
+        public frmInicio()
+        {
+            InitializeComponent();
+
+        }
+
+        private void leerCertificado()
+        {
+            try
+            {
+                //Carga el certificado
+                X509Certificate2 x509 = new X509Certificate2(certificadoPath, password, X509KeyStorageFlags.Exportable);
+
+                //Grabar en un fichero los datos del certificado.
+                using (StreamWriter sw = new StreamWriter(salida))
+                {
+                    string datosCertificado = x509.ToString(true);
+                    string cifSociedad = extraeCadena(datosCertificado, "OID.1.3.6.1.4.1.5734.1.7=VATES-", "\r");
+                    string sociedad = extraeCadena(datosCertificado, "OID.1.3.6.1.4.1.5734.1.6=", "\r");
+                    string apellido1 = extraeCadena(datosCertificado, "OID.1.3.6.1.4.1.5734.1.2=", "\r");
+                    string apellido2 = extraeCadena(datosCertificado, "OID.1.3.6.1.4.1.5734.1.3=", "\r");
+                    string nombre = extraeCadena(datosCertificado, "OID.1.3.6.1.4.1.5734.1.1=", "\r");
+
+                    if (cifSociedad == "")
+                    {
+                        string dni = extraeCadena(datosCertificado, "OID.1.3.6.1.4.1.5734.1.4=", "\r");
+                        sw.WriteLine("Titular: {0} {1}, {2}", apellido1, apellido2, nombre);
+                        sw.WriteLine("NIF: {0}", dni);
+                    }
+                    else
+                    {
+                        string dni = extraeCadena(datosCertificado, "OID.1.3.6.1.4.1.5734.1.4=IDCES-", "\r");
+                        sw.WriteLine("Representante: {0} {1}, {2}", apellido1, apellido2, nombre);
+                        sw.WriteLine("NIF representante: {0}", dni);
+                        sw.WriteLine("Titular certificado: {0}", sociedad);
+                        sw.WriteLine("CIF titular certificado: {0}", cifSociedad);
+                    }
+                    sw.WriteLine("Valido desde: {0}", x509.NotBefore);
+                    sw.WriteLine("Valido hasta: {0}", x509.NotAfter);
+                    sw.WriteLine("Huella digital: {0}", x509.Thumbprint);
+                    sw.WriteLine("Numero de serie: {0}", x509.SerialNumber);
+                }
+            }
+
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Error: El fichero {certificadoPath} no existe.");
+            }
+
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine($"Error: No se puede acceder al fichero {certificadoPath} informado.");
+            }
+
+            catch (CryptographicException ex)
+            {
+                MessageBox.Show($"La contraseña del certificado introducida no es correcta. {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPassword1.Text = "";
+                txtPassword2.Text = "";
+                txtPassword1.Focus();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+
+        private void btnSeleccion_Click(object sender, EventArgs e)
+        {
+            if (ofdSeleccion.ShowDialog() == DialogResult.OK)
+            {
+                // Obtiene la ruta completa del archivo seleccionado
+                certificadoPath = ofdSeleccion.FileName;
+
+                // Actualiza el contenido del TextBox con la ruta del archivo
+                txtSeleccion.Text = certificadoPath;
+
+                txtPassword1.Focus();
+            }
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtPassword1.Text != txtPassword2.Text)
+                {
+                    throw new Exception("Las contraseñas no coinciden.");
+                }
+                password = txtPassword1.Text;
+                leerCertificado();
+                MessageBox.Show("Certificado leido correctamente", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                Application.Exit();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPassword1.Text = "";
+                txtPassword2.Text = "";
+                txtPassword1.Focus();
+            }
+        }
+
+        static string extraeCadena(string input, string inicio, string fin)
+        {
+            string pattern = $"{Regex.Escape(inicio)}(.*?){Regex.Escape(fin)}";
+            Match match = Regex.Match(input, pattern);
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
